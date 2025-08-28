4c6af050ed80cbf9916bcdda70376a3555eb8b5b
Clary-p <clarissapietrangelo@gmail.com>
2025-08-28 16:23:14 -0300

    Update Est.repetição-ex.1 Pg. 117.c

--- Est.repetição-ex.1 Pg. 117.c
+++ Est.repetição-ex.1 Pg. 117.c
@@ -14,6 +14,6 @@ for (i = 2007; i <= ano_atual; i++){
 percentual = 2 * percentual;
 novo_salario = novo_salario + percentual * novo_salario;
 }
-printf("novo salário: %d",novo_salario);
+printf("novo salário:R$ %d",novo_salario);
 return 0;
 }
