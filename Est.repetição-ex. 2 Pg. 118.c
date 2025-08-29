262ce6b1c26ac3f9839bf5ee3c54157049cdbb39
Clary-p <clarissapietrangelo@gmail.com>
2025-08-29 20:15:21 -0300

    Update and rename Est.repetição-ex.1 Pg. 117.c to Est.repetição-ex. 2 Pg. 118.c

---
+++ Est.repetição-ex. 2 Pg. 118.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>  // estou acumulando bibliotecas
+#include <time.h>
+//exercício 2 página 118
+
+int main() {
+    int n, i, j;
+    double fat, e;  //constante de Euler?
+    scanf("%d", &n);
+    e = 1.0;
+    for (i = 1; i <= n; i++) {
+        fat = 1.0;
+        for (j = 1; j <= i; j++) {
+            fat = fat * j;}
+        e = e + 1.0 / fat;}
+    printf("e = %lf\n", e);
+    return 0;}
